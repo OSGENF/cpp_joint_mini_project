d93eba8a2c2f2618a37b14630240bfd63c3ae595
Hugo Artemio Gonzalez <hagonzalez94@gmail.com>
2013-11-30 11:22:47 -0500

    Initial Commit

---
+++ cpp_joint_mini_project/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cpp_joint_mini_project
+//
+//  Created by Hugo on 11/30/13.
+//  Copyright (c) 2013 Golf Squad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
